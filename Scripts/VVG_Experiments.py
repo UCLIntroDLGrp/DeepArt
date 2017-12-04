75f2a5fc626977ef0878272efa45b991c854a369
eugval <Tjoun@eduroam-int-dhcp-97-119-140.ucl.ac.uk>
2017-12-04 17:48:34 +0000

    Experiments pseudocode added'

---
+++ Scripts/VVG_Experiments.py
@@ -0,0 +1,26 @@
+#Load the images with crop 224,224
+#split with train/validation/test
+#Load VGG with imageNet weights and 8 classes
+
+#FUNCTION  evalutate
+# Save model history
+# Save results Top3 and Top1 accuracy on test set
+# Save model visualisations
+
+#Experiemnt1
+# Retrain all layers
+# Run evaluate
+
+#Experiment2
+# Freeze first 3 blocks of VGG
+# Run evaluate
+
+#Experiment3
+# Free everything but last layer
+# Run evaluate
+
+
+
+
+
+
