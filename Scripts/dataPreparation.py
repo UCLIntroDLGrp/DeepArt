d043296dbdde908767c5b3109a33aab37ba93fbe
eugval <Tjoun@eduroam-int-dhcp-97-75-76.ucl.ac.uk>
2017-12-08 14:43:46 +0000

    Decoupled loading data with training

---
+++ Scripts/dataPreparation.py
@@ -0,0 +1,27 @@
+
+import sys
+import os
+import numpy as np
+
+sys.path.insert(0, os.path.realpath('../'))
+from Preprocessing.preprocessing import generate_cropped_training_and_test_data
+
+if __name__ == '__main__':
+    # Get the data:
+    crop_dims = (224, 224)
+    directory = '../Art_Data_sm'
+    number_of_crops = 4
+    validation_size = 10.0 / 100
+    train_size = 80.0 / 100
+    X_train, X_validation, Y_train, Y_validation = generate_cropped_training_and_test_data(directory,
+                                                                               crop_dims,
+                                                                               number_of_crops,
+                                                                               validation_size,
+                                                                               train_size,
+                                                                               10)
+    print("Saving...")
+
+    np.save("X_train", X_train)
+    np.save("X_validation", X_validation)
+    np.save("Y_train", Y_train)
+    np.save("Y_validation", Y_validation)
