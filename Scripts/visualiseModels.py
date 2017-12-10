ac027c9f2d3f95529d877aac349953072a89e326
eugval <Tjoun@Eugenes-MacBook-Pro.local>
2017-12-10 12:52:57 +0000

    adding saving facilities to visualisatoins

--- Scripts/visualiseModels.py
+++ Scripts/visualiseModels.py
@@ -11,12 +11,14 @@ model = load_model('../SavedData/Experiment3Resnet.h5')
 print("Visualising the dense layer....")
 selected_layer ='output_predictions'
 output_classes = 8
-visualiseDenseLayer(model,selected_layer,output_classes, False)
+save_name = "Experiment_3_Resnet_Output_Visualisation.png"
+visualiseDenseLayer(model,selected_layer,output_classes, False, save=save_name)
 
 
 print("Visualising the convolutional layer")
 selected_layer = 'activation_13'
 selected_indices = [361,213,206,151,251]
-visualiseCovolutionLayer(model,selected_layer,selected_indices, False)
+save_name = "Experiment_3_Resnet_Activation_13_Layer_Visualisation.png"
+visualiseCovolutionLayer(model,selected_layer,selected_indices, False, save= save_name)
 
 
