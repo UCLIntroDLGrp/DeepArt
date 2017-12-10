b4c1fc0ba90cdfa298f755c4402e00ece574bf05
eugval <pe.val@icloud.com>
2017-12-10 12:32:43 +0000

    Added verbosity and docstrings to visualisation api

--- Scripts/visualiseModels.py
+++ Scripts/visualiseModels.py
@@ -5,16 +5,18 @@ from keras.models import load_model
 
 from Visualisation.visualisation import visualiseDenseLayer, visualiseCovolutionLayer
 
-
+print("Loading the model in Experiment3Resnet.h5....")
 model = load_model('../SavedData/Experiment3Resnet.h5')
 
+print("Visualising the dense layer....")
 selected_layer ='output_predictions'
 output_classes = 8
-visualiseDenseLayer(model,selected_layer,output_classes)
+visualiseDenseLayer(model,selected_layer,output_classes, False)
 
 
+print("Visualising the convolutional layer")
 selected_layer = 'activation_13'
 selected_indices = [361,213,206,151,251]
-visualiseCovolutionLayer(model,selected_layer,selected_indices)
+visualiseCovolutionLayer(model,selected_layer,selected_indices, False)
 
 
--- Visualisation/visualisation.py
+++ Visualisation/visualisation.py
@@ -5,13 +5,23 @@ from keras import activations
 from vis.input_modifiers import Jitter
 
 
-def visualiseDenseLayer(model,layer_name,output_classes):
+def visualiseDenseLayer(model,layer_name,output_classes, verbose):
+    '''
+    Makes a plot for visualising  the activations of a dense layer.
+    :param model:  The model to visualise.
+    :param layer_name: The name of the dense layer to visualise.
+    :param output_classes: The number of activations in the layer.
+    :param verbose: Print statements of progress.
+    :return: N/A
+    '''
     layer_index = utils.find_layer_idx(model, layer_name)
     model.layers[layer_index].activation = activations.linear
     model = utils.apply_modifications(model)
 
     vis_images = []
     for filter_index in range(0, output_classes):
+        if(verbose):
+            print("Preparing Visualisation for class {} in layer {}".format(filter_index,layer_name))
         visualisation = visualize_activation(model, layer_index, filter_index, max_iter=500, input_modifiers=[Jitter(16)])
 
         img = utils.draw_text(visualisation, 'Class {}'.format(filter_index))
@@ -25,10 +35,20 @@ def visualiseDenseLayer(model,layer_name,output_classes):
     plt.show()
 
 
-def visualiseCovolutionLayer(model,selected_layer,selected_indices):
+def visualiseCovolutionLayer(model,selected_layer,selected_indices, verbose):
+    '''
+    Makes a plot for visualising  the activations of a convolutional layer.
+    :param model: The model to visualise.
+    :param selected_layer: The name of the dense layer to visualise.
+    :param selected_indices: The indices of the filters to visualise.
+    :param verbose: Print statements of progress.
+    :return: N/A.
+    '''
     layer_index = utils.find_layer_idx(model, selected_layer)
     vis_images = []
     for filter_index in selected_indices:
+        if (verbose):
+            print("Preparing Visualisation for filter {} in layer {}".format(filter_index,selected_layer))
         img = visualize_activation(model, layer_index, filter_index)
         # Utility to overlay text on image.
         visualisation = utils.draw_text(img, 'Filter {} on layer {}'.format(filter_index,selected_layer))
