73c9a25351e152e1afbd1d4ae5210425684914b1
Liam <kingliam28@ntlworld.com>
2017-12-14 22:52:14 +0000

    Save images before cropping

--- Scripts/dataPreparation.py
+++ Scripts/dataPreparation.py
@@ -2,9 +2,9 @@
 import sys
 import os
 import numpy as np
+from Preprocessing.preprocessing import generate_training_and_test_data
 
 sys.path.insert(0, os.path.realpath('../'))
-from Preprocessing.preprocessing import generate_cropped_training_and_test_data
 
 if __name__ == '__main__':
     # Get the data:
@@ -13,12 +13,10 @@ if __name__ == '__main__':
     number_of_crops = 4
     validation_size = 10.0 / 100
     train_size = 80.0 / 100
-    X_train, X_validation, Y_train, Y_validation = generate_cropped_training_and_test_data(directory,
-                                                                               crop_dims,
-                                                                               number_of_crops,
-                                                                               validation_size,
-                                                                               train_size,
-                                                                               10)
+    X_train, X_validation, Y_train, Y_validation = generate_training_and_test_data(directory,
+                                                                                   validation_size,
+                                                                                   train_size,
+                                                                                   image_selection_percent=10)
     print("Saving...")
 
     np.save("../SavedData/X_train", X_train)
