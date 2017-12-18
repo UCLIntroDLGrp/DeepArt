05d37a0998b42ca9f84476ddf98f5d8dab869f5d
Liam <kingliam28@ntlworld.com>
2017-12-18 22:41:56 +0000

    Assign variable names to load in

--- Scripts/Resnet_50_Experiments.py
+++ Scripts/Resnet_50_Experiments.py
@@ -55,10 +55,10 @@ if __name__ == '__main__':
                                                                                                10)
 
     else:
-        np.load("../../../../../ml/2017/DeepArt/SavedData/X_train.npy")
-        np.load("../../../../../ml/2017/DeepArt/SavedData/X_validation.npy")
-        np.load("../../../../../ml/2017/DeepArt/SavedData/Y_train.npy")
-        np.load("../../../../../ml/2017/DeepArt/SavedData/Y_validation.npy")
+        X_train = np.load("../../../../../ml/2017/DeepArt/SavedData/X_train.npy")
+        X_validation = np.load("../../../../../ml/2017/DeepArt/SavedData/X_validation.npy")
+        Y_train = np.load("../../../../../ml/2017/DeepArt/SavedData/Y_train.npy")
+        Y_validation = np.load("../../../../../ml/2017/DeepArt/SavedData/Y_validation.npy")
 
         X_train, X_validation, Y_train, Y_validation = crop_data_from_load(
             X_train, X_validation, Y_train, Y_validation, crop_dims, number_of_crops)
