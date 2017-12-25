21fe4846cbf1e26d8ed341802d98450ba65894d6
eugval <pe.val@icloud.com>
2017-12-25 19:45:25 +0200

    bug fix

--- Scripts/Resnet_50_Experiments.py
+++ Scripts/Resnet_50_Experiments.py
@@ -78,7 +78,7 @@ if __name__ == '__main__':
     # Hyperparameters
     batch_size = 128
     nb_epoch = 40
-    patience = 4
+    patience = 3
     num_classes = 7
     loss = 'categorical_crossentropy'
     metrics = ['accuracy']
