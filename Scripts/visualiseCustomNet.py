98c73801e596bd44de08a79e6a7d458d1c405d2a
eugval <pe.val@icloud.com>
2018-01-02 18:33:58 +0200

    custom vis

--- Scripts/visualiseCustomNet.py
+++ Scripts/visualiseCustomNet.py
@@ -1,26 +1,23 @@
 import os
 import sys
 sys.path.insert(0, os.path.realpath('../'))
-from Visualisation.layerVisualisation import visualiseDenseLayer
+from Visualisation.layerVisualisation import visualiseGenericLayer
 from CustomNet.CustomNet import customModel
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 
-MODEL_METADATA =[('../SavedData/Experiment1CustomNetWeights.h5 ',"Experiment_1_CustomNoDrop_Output_Visualisation.png")]
+MODEL_METADATA =[('../SavedData/Experiment1CustomNet.h5',"Experiment_1_customNetNoDropout_Output_Visualisation.png")]
 
 num_classes = 7
-num_routing = 3
-input_shape = [40,40,3]
+input_shape = [224,224,3]
 
 for model_file, figure_save_name in MODEL_METADATA:
     print("Loading the model from {}...".format(model_file))
-    model = customModel(input_shape=input_shape,
-                n_class=num_classes,
-                num_routing=num_routing)
+    model = customModel(input_shape, num_classes,False)
 
     model.load_weights(model_file)
 
     print("Visualising the dense layer...")
     selected_layer = 'out_caps'
     output_classes = 7
-    visualiseDenseLayer(model,selected_layer,output_classes, True, save=figure_save_name)
+    visualiseGenericLayer(model,selected_layer,output_classes, True, save=figure_save_name)
