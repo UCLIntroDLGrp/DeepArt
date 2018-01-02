5fdda5966de771cf31cacc77c624f6801f410027
eugval <pe.val@icloud.com>
2018-01-02 10:43:01 +0200

    script change

--- Scripts/visualiseDenseLayers.py
+++ Scripts/visualiseDenseLayers.py
@@ -5,7 +5,8 @@ from keras.models import load_model
 from Visualisation.layerVisualisation import visualiseDenseLayer, visualiseCovolutionLayer
 
 
-MODEL_METADATA =[('../../Experiment1Resnet.h5',"Experiment_1_Resnet_Output_Visualisation.png"),('../../Experiment2Resnet.h5',"Experiment_2_Resnet_Output_Visualisation.png"),('../../Experiment3Resnet.h5',"Experiment_3_Resnet_Output_Visualisation.png")]
+#MODEL_METADATA =[('../../Experiment1Resnet.h5',"Experiment_1_Resnet_Output_Visualisation.png"),('../../Experiment2Resnet.h5',"Experiment_2_Resnet_Output_Visualisation.png"),('../../Experiment3Resnet.h5',"Experiment_3_Resnet_Output_Visualisation.png")]
+MODEL_METADATA =[('../SavedData/Experiment2Resnet2.h5',"Experiment_2_Resnet2_Output_Visualisation.png")]
 
 for model_file, figure_save_name in MODEL_METADATA:
     print("Loading the model from {}...".format(model_file))
