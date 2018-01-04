72e84f084c5089dcc1eccdef2c392e7f8c97014b
Liam <kingliam28@ntlworld.com>
2018-01-04 12:57:39 +0000

    Change preprocessing to work on test

--- Scripts/dataPreparation.py
+++ Scripts/dataPreparation.py
@@ -9,17 +9,16 @@ from Preprocessing.preprocessing import generate_training_and_test_data
 if __name__ == '__main__':
     # Get the data:
     crop_dims = (224, 224)
-    directory = '../Test_data'
+    directory = '../Art_Data_sm'
     number_of_crops = 1
     validation_size = 10.0 / 100
     train_size = 80.0 / 100
-    X_train, X_validation, Y_train, Y_validation = generate_training_and_test_data(directory,
-                                                                                   validation_size,
-                                                                                   train_size,
-                                                                                   image_selection_percent=10)
+    X_test, Y_test = generate_training_and_test_data(directory,
+                                                     validation_size,
+                                                     train_size,
+                                                     image_selection_percent=10,
+                                                     test_set=True)
     print("Saving...")
 
-    np.save("../../../../../ml/2017/DeepArt/SavedData/X_train.npy", X_train)
-    np.save("../../../../../ml/2017/DeepArt/SavedData/X_validation.npy", X_validation)
-    np.save("../../../../../ml/2017/DeepArt/SavedData/Y_train.npy", Y_train)
-    np.save("../../../../../ml/2017/DeepArt/SavedData/Y_validation.npy", Y_validation)
+    np.save("../../../../../ml/2017/DeepArt/SavedData/X_test.npy", X_test)
+    np.save("../../../../../ml/2017/DeepArt/SavedData/Y_test.npy", Y_test)
